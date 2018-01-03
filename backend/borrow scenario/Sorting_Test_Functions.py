8abec11ce07d7b05d6580948190f71ae3f5073eb
AminaEmenena <34988195+AminaEmenena@users.noreply.github.com>
2018-01-02 22:59:49 -0800

    Add files via upload

--- backend/borrow scenario/Sorting_Test_Functions.py
+++ backend/borrow scenario/Sorting_Test_Functions.py
@@ -2,7 +2,9 @@
 import sys
 import operator
 
-#
+#customer attributes are considered favorable in the following order.
+#excluding name data
+
 
 #attribute 0:'First Name',
 #attribute 1::'Last Name',
@@ -12,33 +14,26 @@ import operator
 #attribute 5:'Credit Score'
 
 #List of Digital Reserve Clients
-s =[['Sally', 'Henderson', '750', '4', '12', '738'],
-['Henry', 'James', '375', '28', '22', '800'],
-['Jeff', 'Madison', '250', '24', '16', '642'],
-['Marcus', 'Mills', '400', '3', '21', '723'],
-['Nate', 'Bradshaw', '120', '20', '19', '825'],
-['Amanda', 'Jefferson', '145', '8', '1', '714'],
-['Jessica', 'Jones', '75', '6', '17', '660'],
-['Alexandra', 'Harris', '1000', '7', '14', '764'],
-['Mason', 'Viri', '1250', '19', '25', '807'],
-['Jed', 'Chan', '950', '23', '5', '593'],
-['David', 'Mitts', '780', '12', '23', '803'],
-['Justin', 'Jackson', '50', '18', '11', '605'],
-['Rick', 'Stone', '1400', '16', '18', '755'],
-['Ed', 'Sanderson', '570', '30', '13', '711'],
-['Ashley', 'Willis', '375', '27', '7', '698'],
-['Kathryn', 'London', '800', '26', '4', '582'],
-['Jignesh', 'Dashi', '650', '5', '24', '616'],
-['Kendrick', 'Blicks', '330', '11', '15', '850'],
-['Molly', 'James', '195', '15', '3', '679'],
-['Issa', 'Rae', '45', '21', '9', '681'],
-['Riley', 'Banks', '30', '10', '20', '771'],
-['Iman', 'Hill', '930', '13', '6', '757'],
-['Ted', 'Williams', '870', '14', '8', '695'],
-['Will', 'Smith', '675', '22', '2', '601']]
-
+s =[['Sally', 'Henderson', 750.00, 4, 12, 738],
+['Henry', 'James', 375.00, 3, 22, 800],
+['Jeff', 'Madison', 250.00, 24, 16, 642],
+['Marcus', 'Mills', 400.00, 3, 21, 723]]
 #Sort by attributes 1 and 
 
-s = sorted(s, key = operator.itemgetter(2),reverse = True)
+#sorts for the lowest loan amount
+s = sorted(s, key = operator.itemgetter(2))
+print(s)
 
+#sorts for the lowest loan period in ages
+s = sorted(s, key = operator.itemgetter(3))
+print(s)
+
+#sorts for the highest interest rate
+s = sorted(s, key = operator.itemgetter(4), reverse=True)
+print(s)
+
+#sorts for the highest credit score
+s = sorted(s, key = operator.itemgetter(5), reverse=True)
 print (s)
+
+print ("Loan Bid Was Won By: ", s[0])
