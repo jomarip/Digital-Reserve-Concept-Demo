9492b369be26a558d46bf2e082b76ff526f60756
Emmanuel <emmanueljohnson609@gmail.com>
2018-01-08 09:57:07 -0800

    upload of flask server with sample login feature

--- flask_project/app/__init__.py
+++ flask_project/app/__init__.py
@@ -1,16 +1,15 @@
-# app/__init__.py 
+from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy 
+from flask_migrate import Migrate 
+from flask_login import LoginManager 
+app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+login = LoginManager(app)
+login.login_view = 'login'
 
-from flask import Flask 
+from app import routes, models 
 
-#Initialize the app 
-
-app = Flask(__name__, instance_relative_config=True)
-
-# Load the views
-
-from app import views
-
-# Load the config file
-
-app.config.from_object('config')
 
--- flask_project/run.py
+++ flask_project/run.py
@@ -1,8 +1,8 @@
-# run.py 
+from app import app, db 
+from app.models import User, Post 
 
-from app import app 
-
-if __name__ == '__main__':
-    app.run()
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User':User, 'Post':Post}
 
 
