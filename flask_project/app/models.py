8da9cdc44e769b105b1c45fe54c13dfbf5c0080b
Emmanuel <emmanueljohnson609@gmail.com>
2018-01-08 11:19:24 -0800

    updated models an incorporated public key generation into registration step

--- flask_project/app/models.py
+++ flask_project/app/models.py
@@ -5,10 +5,11 @@ from flask_login import UserMixin
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key = True)
-    username = db.Column(db.String(64), index=True, unique=True)
+    public_key = db.Column(db.String(64), index=True, unique=True)
+    private_key = db.Column(db.String(64), index=True, unique=True) 
     email = db.Column(db.String(120), index=True,unique=True)
     password_hash = db.Column(db.String(128))
-    posts = db.relationship('Post', backref='author', lazy = 'dynamic')
+    balance = db.Column(db.Integer)
 
     def __repr__(self):
     	 return '<User {}>'.format(self.username)
@@ -16,16 +17,6 @@ class User(UserMixin, db.Model):
         self.password_hash = generate_password_hash(password)
     def check_password(self, password): 
         return check_password_hash(self.password_hash, password) 
-
-class Post(db.Model): 
-    id = db.Column(db.Integer, primary_key=True)
-    body = db.Column(db.String(140))
-    timestamp = db.Column(db.DateTime, index=True, default = datetime.utcnow)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-
-    def __repr__(self):
-        return '<Post {}>'.format(self.body)
-
 
 from app import login 
 
